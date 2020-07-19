f20854d7c2594832842564429a426463e7a9103c
Derek Adams <derek.adams@sitewhere.com>
2020-07-19 11:12:02 -0400

    fix: Bump version

--- dist/api/devices-api.d.ts
+++ dist/api/devices-api.d.ts
@@ -1,5 +1,5 @@
 import { AxiosInstance, AxiosPromise } from "axios";
-import { IDeviceCreateRequest, IDevice, IDeviceSearchCriteria, IDeviceResponseFormat, IDeviceSearchResults } from "../model/devices-model";
+import { IDeviceCreateRequest, IDevice, IDeviceSearchCriteria, IDeviceResponseFormat, IDeviceSearchResults, IDeviceSummarySearchResults } from "../model/devices-model";
 import { IDeviceAssignmentResponseFormat, IDeviceAssignmentSearchResults } from "../model/device-assignments-model";
 import { ISearchCriteria } from "../model/common-model";
 /**
@@ -34,7 +34,7 @@ export declare function listDevices(axios: AxiosInstance, criteria?: IDeviceSear
  * @param criteria
  * @param format
  */
-export declare function listDeviceSummaries(axios: AxiosInstance, criteria?: IDeviceSearchCriteria): AxiosPromise<IDeviceSearchResults>;
+export declare function listDeviceSummaries(axios: AxiosInstance, criteria?: IDeviceSearchCriteria): AxiosPromise<IDeviceSummarySearchResults>;
 /**
  * Delete an existing device.
  * @param axios
